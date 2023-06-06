a32cb91a590ec361511637971001b8d61e11cf6c
pradanadp <pradana.dimasdp@gmail.com>
2023-06-06 18:23:33 +0700

    Add initial data for user, team, and status

--- app/database/mysql.go
+++ app/database/mysql.go
@@ -2,13 +2,15 @@ package database
 
 import (
 	"alta-immersive-dashboard/app/config"
-	classModel "alta-immersive-dashboard/features/class/repository"
-	feedbackModel "alta-immersive-dashboard/features/feedback/repository"
-	menteeModel "alta-immersive-dashboard/features/mentee/repository"
-	statusModel "alta-immersive-dashboard/features/status/repository"
-	teamModel "alta-immersive-dashboard/features/team/repository"
-	userModel "alta-immersive-dashboard/features/user/repository"
+	classRepo "alta-immersive-dashboard/features/class/repository"
+	feedbackRepo "alta-immersive-dashboard/features/feedback/repository"
+	menteeRepo "alta-immersive-dashboard/features/mentee/repository"
+	"alta-immersive-dashboard/utils"
 	"fmt"
+
+	statusRepo "alta-immersive-dashboard/features/status/repository"
+	teamRepo "alta-immersive-dashboard/features/team/repository"
+	userRepo "alta-immersive-dashboard/features/user/repository"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -26,10 +28,58 @@ func InitMysql(cfg *config.AppConfig) *gorm.DB {
 }
 
 func InitialMigration(DB *gorm.DB) {
-	DB.AutoMigrate(&feedbackModel.Feedback{})
-	DB.AutoMigrate(&menteeModel.Mentee{})
-	DB.AutoMigrate(&statusModel.Status{})
-	DB.AutoMigrate(&classModel.Class{})
-	DB.AutoMigrate(&userModel.User{})
-	DB.AutoMigrate(&teamModel.Team{})
+	DB.AutoMigrate(&feedbackRepo.Feedback{})
+	DB.AutoMigrate(&menteeRepo.Mentee{})
+	DB.AutoMigrate(&statusRepo.Status{})
+	DB.AutoMigrate(&classRepo.Class{})
+	DB.AutoMigrate(&userRepo.User{})
+	DB.AutoMigrate(&teamRepo.Team{})
+}
+
+func InitialUserData(db *gorm.DB) {
+	user := userRepo.User{
+		TeamID:   1,
+		FullName: "John Doe",
+		Email:    "johndoe@mail.com",
+		Password: "qwerty",
+		Status:   "active",
+		Role:     "admin",
+	}
+
+	user.Password = utils.HashPass(user.Password)
+
+	db.Create(&user)
+}
+
+func InitialTeamData(db *gorm.DB) {
+	teams := []teamRepo.Team{
+		{TeamName: "Manager"},
+		{TeamName: "Mentor"},
+		{TeamName: "Placement Team"},
+		{TeamName: "People Skill Team"},
+	}
+
+	for _, team := range teams {
+		db.Create(&team)
+	}
+}
+
+func InitialStatusData(db *gorm.DB) {
+	statuses := []statusRepo.Status{
+		{StatusName: "Interview"},
+		{StatusName: "Join Class"},
+		{StatusName: "Unit 1"},
+		{StatusName: "Unit 2"},
+		{StatusName: "Unit 3"},
+		{StatusName: "Repeat Unit 1"},
+		{StatusName: "Repeat Unit 2"},
+		{StatusName: "Repeat Unit 3"},
+		{StatusName: "Placement"},
+		{StatusName: "Eliminated"},
+		{StatusName: "Graduated"},
+	}
+
+	for _, status := range statuses {
+		db.Create(&status)
+	}
 }
